7b2a310ac3338e9018d32032269975e5cb442cd2
Rolf Haug <rolf@rah.no>
2018-10-17 09:39:22 +0200

    Remove solution

--- main.js
+++ main.js
@@ -15,22 +15,29 @@ function getArticles()
 function filter(event)
 {
   // @TODO: get search term from event
+  const term = event.target.value
 
   // @TODO: check if search term is long enough
+  if(term.length < 3) return showOriginalArticles()
 
   // @TODO: Filter articles
+  const filteredArticles = filterArticles(getArticles(), term)
 
   // @TODO: update DOM (Insert filtered articles in HTML)
+  document.getElementById('articles').innerHTML = filteredArticles.join("")
 }
 
 // Filter the articles
 function filterArticles(articles, term)
 {
   // @TODO: create a filter that returns articles where the search term exists
+  articles = articles.filter(element => element.innerText.match(term))
 
   // @TODO: Highlight search terms with
+  articles = articles.map(element => highlight(element.innerHTML, term))
 
   // @TODO: return filtered and highlighted articles
+  return articles;
 }
 
 // Highlight the search term occurences
@@ -44,13 +51,18 @@ function highlight(string, term)
 function showOriginalArticles()
 {
   // @TODO: create HTML from the original articles
+  const html = getArticles().map(article => article.innerHTML)
 
   // @TODO: update the DOM with the HTML
+  document.getElementById('articles').innerHTML = html.join("")
 }
 
 // Clear input field and show original articles
 function removeFilters()
 {
   // @TODO: clear search term
+  document.querySelector('.filter input').value = ''
+  
   // @TODO: show original articles in the DOM
+  showOriginalArticles()
 }
